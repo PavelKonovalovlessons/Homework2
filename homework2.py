342e5fedd6253e58df12e4a451cbe32c351c5400
PavelKonovalovlessons <konovnovv@mail.ru>
2024-07-29 13:09:18 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,10 @@
+complete_homework = 12
+
+hours = 1.5
+
+course = "Python"
+
+hours_for_one_task = hours / complete_homework
+
+print('Курс:', course, ', всего задач:', complete_homework, ', затрачено часов:', hours,
+      ', среднее время выполнения:', hours_for_one_task, 'часа')
